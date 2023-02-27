d0746fe515b776c427fc64da80fba9775ec27c40
Lawrence <lawrencebaron027@gmail.com>
2023-02-26 18:45:13 -0700

    Added insert and delete on books in front page

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useBookContext } from "../../hooks/useBookContext";
+import Button from "../forms/Button";
+import Label from "../forms/Label";
+import Alert from "../forms/Alert";
+import Input from "../forms/Input";
 
 const Home = () => {
-  const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [genre, setGenre] = useState("");
+  const [publisher, setPublisher] = useState("");
+  const [pages, setPages] = useState("");
+  const [error, setError] = useState([]);
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  const { books, dispatch } = useBookContext();
   useEffect(() => {
     fetchBooks();
   }, []);
@@ -12,13 +27,66 @@ const Home = () => {
     const response = await axios
       .get("http://localhost:4000/api/book")
       .then((res) => {
-        setBooks(res.data.data);
-        console.log(res.data.data);
+        dispatch({ type: "FETCH_BOOKS", payload: res.data.data });
       })
       .catch((err) => {
         console.log("Error: ", err);
       });
     // const data = await response.json();
+  };
+
+  const deleteBook = async (id) => {
+    await axios
+      .delete(`http://localhost:4000/api/book/${id}`)
+      .then((res) => {
+        dispatch({ type: "DELETE_BOOK", payload: id });
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const book = {
+      title,
+      author,
+      description,
+      price,
+      publisher,
+      pages,
+      genre,
+    };
+    addBook(book);
+  };
+
+  const addBook = async (book) => {
+    await axios
+      .post("http://localhost:4000/api/book", book)
+      .then((res) => {
+        setTimeout(() => {
+          setTitle("");
+          setAuthor("");
+          setDescription("");
+          setPrice("");
+          setPublisher("");
+          setPages("");
+          setGenre("");
+          dispatch({ type: "CREATE_BOOK", payload: res.data.data });
+          console.log(res.data.data);
+          setSuccess(true);
+          setError([]);
+          setLoading(false);
+        }, 500);
+      })
+      .catch((err) => {
+        setTimeout(() => {
+          setLoading(false);
+          setSuccess(false);
+          setError(err.response.data.data);
+        }, 500);
+      });
   };
 
   return (
@@ -29,7 +97,7 @@ const Home = () => {
             <div key={book._id} className="w-full">
               <div className="bg-white shadow-md rounded-lg overflow-hidden m-2 h-full">
                 <div className="px-8 py-6 space-y-2">
-                  <h1 className="text-gray-900 font-bold text-2xl">
+                  <h1 className="text-slate-700 font-bold text-2xl">
                     {book.title}
                   </h1>
                   <p className="text-gray-600 text-sm mt-1">{book.author}</p>
@@ -39,10 +107,121 @@ const Home = () => {
                   </p>
                   <p className="text-gray-600 text-sm mt-1">{book.price}</p>
                   <p className="text-gray-600 text-sm mt-1">{book.genre}</p>
+                  <Button
+                    onClick={() => deleteBook(book._id)}
+                    className={"bg-slate-700 py-2 px-4 rounded"}
+                  >
+                    Delete
+                  </Button>
                 </div>
               </div>
             </div>
           ))}
+      </div>
+      <div className="flex my-10">
+        <div className="bg-white shadow-md rounded-lg overflow-hidden m-2 h-full py-5">
+          <form onSubmit={handleSubmit}>
+            <div className="px-8 py-6 space-y-1">
+              {error && error.length > 0 ? (
+                <Alert className={"bg-red-500"}>
+                  {Array.isArray(error) ? (
+                    error.map((err, index) => (
+                      <p key={index} className="text-white">
+                        {err}
+                      </p>
+                    ))
+                  ) : (
+                    <p className="text-white">{error}</p>
+                  )}
+                </Alert>
+              ) : (
+                ""
+              )}
+              {success ? (
+                <Alert className={"bg-green-500"}>
+                  <p className="text-white">Book successfully added!</p>
+                </Alert>
+              ) : (
+                ""
+              )}
+
+              <h1 className="text-slate-700 font-bold text-2xl">Add Book</h1>
+              <div className="grid grid-cols-3 gap-1">
+                <div className="my-1">
+                  <Label htmlFor="title">Title</Label>
+                  <Input
+                    type="text"
+                    id="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+                <div className="my-1">
+                  <Label htmlFor="author">Author</Label>
+                  <Input
+                    type="text"
+                    id="author"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                  />
+                </div>
+                <div className="my-1">
+                  <Label htmlFor="description">Description</Label>
+                  <Input
+                    type="text"
+                    id="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+                <div className="my-1">
+                  <Label htmlFor="price">Price</Label>
+                  <Input
+                    type="text"
+                    id="price"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                  />
+                </div>
+                <div className="my-1">
+                  <Label htmlFor="genre">Genre</Label>
+                  <Input
+                    type="text"
+                    id="genre"
+                    value={genre}
+                    onChange={(e) => setGenre(e.target.value)}
+                  />
+                </div>
+                <div className="my-1">
+                  <Label htmlFor="publisher">Publisher</Label>
+                  <Input
+                    type="text"
+                    id="publisher"
+                    value={publisher}
+                    onChange={(e) => setPublisher(e.target.value)}
+                  />
+                </div>
+                <div className="my-1">
+                  <Label htmlFor="pages">Pages</Label>
+                  <Input
+                    type="text"
+                    id="pages"
+                    value={pages}
+                    onChange={(e) => setPages(e.target.value)}
+                  />
+                </div>
+                <Button
+                  type="submit"
+                  className={
+                    "bg-slate-700 col-span-3 py-2 px-4 rounded float-right "
+                  }
+                >
+                  {loading ? "Loading..." : "Add"}
+                </Button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
