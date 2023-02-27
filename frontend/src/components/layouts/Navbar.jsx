5b6197cc7a91ea533a383eb8fcf4259e160254b2
Lawrence <lawrencebaron027@gmail.com>
2023-02-26 18:45:22 -0700

    Added conditional routes

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -1,6 +1,22 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+
+    // Redirect to Login page
+    navigate("/login");
+  };
+
   return (
     // fixed navbar
     <div className="w-full h-16 bg-gray-800 text-white flex items-center justify-between px-4 fixed top-0">
@@ -22,12 +38,27 @@ const Navbar = () => {
       </div>
       <div className="flex items-center">
         <ul className="flex items">
-          <li className="mx-2">
-            <Link to="/login">Login</Link>
-          </li>
-          <li className="mx-2">
-            <Link to="/register">Register</Link>
-          </li>
+          {user && (
+            <>
+              <li className="mx-2">
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li className="mx-2 cursor-pointer" onClick={handleLogout}>
+                Logout
+              </li>
+            </>
+          )}
+
+          {!user && (
+            <>
+              <li className="mx-2">
+                <Link to="/login">Login</Link>
+              </li>
+              <li className="mx-2">
+                <Link to="/register">Register</Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </div>
